fdb49023923cbb973c8e7a7b7436fe221eff95c1
inwooshin <dkrlehowl45@naver.com>
2021-07-24 12:33:46 +0900

    자바 기본 사칙연산

---
+++ src/add_sub_min_div.java
@@ -0,0 +1,13 @@
+public class add_sub_min_div {
+    public static void main(String ar[]){
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + ((float) a / b));
+            //이런 식으로 사칙연산을 할 수 있으며 float 자료형을 사용할 수 있다.
+
+    }
+}
