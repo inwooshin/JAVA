004eb3df6986fc1437b03038beb1e8170735a780
inwooshin <dkrlehowl45@naver.com>
2021-10-11 17:12:21 +0900

    poly peach를 ploy fruit 을  상속

---
+++ src/poly_peach.java
@@ -0,0 +1,11 @@
+public class poly_peach extends ploy_fruit{
+    public poly_peach(){
+        price = 1500;
+        name = "복숭아";
+        fresh = 75;
+    }
+
+    public void show(){
+        System.out.println("my name is peach");
+    }
+}
