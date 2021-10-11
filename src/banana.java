97488a264de3c4000318275474b19231af96073c
inwooshin <dkrlehowl45@naver.com>
2021-10-12 08:42:56 +0900

    바나나로 ploy_fruit 상속

---
+++ src/banana.java
@@ -0,0 +1,12 @@
+public class banana extends ploy_fruit{
+
+    public banana(){
+        name = "바나나";
+        price = 3000;
+        fresh = 10;
+    }
+
+    public void show(){
+        System.out.println("my name! banana");
+    }
+}
