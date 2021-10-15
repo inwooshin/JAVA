b35ff92c066b23df15953f56ed8c45187c631e49
inwooshin <dkrlehowl45@naver.com>
2021-10-15 14:33:49 +0900

    객체 지향 공부 archer 클래스 생성

---
+++ src/oop_archer.java
@@ -0,0 +1,9 @@
+public class oop_archer extends oop_hero{
+    public oop_archer(String name) {
+        super(name);
+    }
+
+    public void fireArrow(){
+        System.out.println("불화살!");
+    }
+}
