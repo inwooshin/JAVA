15430d61c71bd3b4a10fa90a163297d9358cc20c
inwooshin <dkrlehowl45@naver.com>
2021-10-23 14:21:07 +0900

    String 만들고 배열 출력

---
+++ src/number_String_Sentence.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public int solution(String s) {
+        String[] str = {"zero","one", "two",
+                "three", "four", "five",
+                "six", "seven", "eight",
+                "nine"};
+        String[] num = {"0","1", "2", "3", "4",
+                "5", "6", "7", "8", "9"};
+
+        for(int i = 0 ; i < 10 ; i++) {
+            System.out.println(s.compareTo(str[i]));
+        }
+
+        return 0;
+    }
+}
