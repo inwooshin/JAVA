f7d6fecb31b6f2b63543152fed088bea7f1dee13
inwooshin <dkrlehowl45@naver.com>
2021-11-27 09:17:00 +0900

    추상화 클래스 설계 테스트

---
+++ src/dog_cat_test.java
@@ -0,0 +1,10 @@
+public class dog_cat_test {
+    public static void main(String ar[]){
+        cat a = new cat();
+        dog b = new dog();
+
+        a.crying();
+        b.crying();
+
+    }
+}
